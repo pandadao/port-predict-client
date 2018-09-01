b6e5ac4be88cd5b20c0741176a818e10fb1d07a5
pandadao <cgsh112@gmail.com>
2018-09-01 15:19:53 +0800

    tired

--- TP-registered.py
+++ TP-registered.py
@@ -8,7 +8,10 @@ def Get_Mac_Value():
     mac = get_mac()
     return mac
 
-
+def Save_Information(Device_ID, Device_password):
+    f = open('IDuse.txt', 'w')
+    f.write(Device_ID +" ")
+    f.write(Device_password)
 
 # Connect to server2 get the device ID and password. the password will change after a service is closed.
 def Register_to_Server2():
@@ -22,11 +25,12 @@ def Register_to_Server2():
     s.send(mac_address)
 
     Device_ID, Device_password = s.recv(1024).split(" ")
-    print "The device ID and password is "+ Device_ID + " " + Device_password
-
+    Save_Information(Device_ID, Device_password)
+    print "The device ID and password is "+ Device_ID + " " + Device_password   #Need to save this information
     s.close
 
 def main():
+    Register_to_Server2()
 
 
 if __name__ == "__main__":
